397d396d7dbc860c39b911530dbe3b07678abc46
Sophia Thapa <sophia.thapa55@gmail.com>
2025-08-08 16:50:16 +0545

    feat: add authentication in all the blog routes

--- blogs/routers/authentication.py
+++ blogs/routers/authentication.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from ..schemas import Login
 from sqlalchemy.orm import Session
 from ..database import get_db
@@ -13,7 +14,7 @@ router = APIRouter(
 )
 
 @router.post('/',status_code=status.HTTP_200_OK)
-def login(request: Login , db : Session = Depends(get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends() , db : Session = Depends(get_db)):
   user = db.query(models.User).filter(models.User.name == request.username).first()
   if not user :
     raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail = 'Invalid Credentials')
--- blogs/routers/blog.py
+++ blogs/routers/blog.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException, Depends, status,APIRouter 
-from .. import models, schemas
+from .. import models, schemas 
+from ..oauth2 import get_current_user
 from ..database import get_db
 from sqlalchemy.orm import Session
 
@@ -9,7 +10,7 @@ router = APIRouter(
 )
 
 @router.post('/',status_code= status.HTTP_201_CREATED)
-def create(request : schemas.Blog, db: Session = Depends(get_db)):
+def create(request : schemas.Blog, db: Session = Depends(get_db),current_user: schemas.User = Depends(get_current_user)):
   new_blog = models.Blog(title = request.title , body=request.body, user_id = 1)
   db.add(new_blog)
   db.commit()
@@ -18,12 +19,12 @@ def create(request : schemas.Blog, db: Session = Depends(get_db)):
 
 
 @router.get('/',response_model=list[schemas.ShowBlog])
-def all(db: Session = Depends(get_db)):
+def all(db: Session = Depends(get_db),current_user: schemas.User = Depends(get_current_user)):
   blog = db.query(models.Blog).all()
   return blog
 
-@router.get('/{id}',response_model= schemas.ShowBlog,tags=['Blog'])
-def show(id, db: Session= Depends(get_db)):
+@router.get('/{id}',response_model= schemas.ShowBlog)
+def show(id, db: Session= Depends(get_db),current_user: schemas.User = Depends(get_current_user)):
   blog = db.query(models.Blog).filter(models.Blog.id == id).first()
   if not blog:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The blog with id {id} is not available")
@@ -32,7 +33,7 @@ def show(id, db: Session= Depends(get_db)):
 
 
 @router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)
-def remove(id,db: Session = Depends(get_db)):
+def remove(id,db: Session = Depends(get_db),current_user: schemas.User = Depends(get_current_user)):
   blog = db.query(models.Blog).filter(models.Blog.id == id)
   if not blog.first():
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The blog with id {id} is not available")
@@ -41,7 +42,7 @@ def remove(id,db: Session = Depends(get_db)):
   return f"the blog of id {id} is deleleted"
 
 @router.put('/{id}',status_code=status.HTTP_202_ACCEPTED)
-def update(id,request: schemas.Blog, db: Session = Depends(get_db)):
+def update(id,request: schemas.Blog, db: Session = Depends(get_db),current_user: schemas.User = Depends(get_current_user)):
   blog = db.query(models.Blog).filter(models.Blog.id == id)
   if not blog.first():
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The blog with id {id} is not available")
